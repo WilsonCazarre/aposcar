b5ee2d1ad12d953db871421d9859e945542b14ae
WilsonCazarre <wcs0486@gmail.com>
2024-10-04 17:47:13 -0300

    initial nominations schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,13 +1,13 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
-  index,
+  boolean,
   pgTableCreator,
-  serial,
-  timestamp,
-  varchar,
+  primaryKey,
+  text,
+  uuid,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -18,19 +18,58 @@ import {
  */
 export const createTable = pgTableCreator((name) => `aposcar_${name}`);
 
-export const posts = createTable(
-  "post",
+export const category = createTable("categories", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  slug: text("slug"),
+  name: text("name"),
+  description: text("description"),
+});
+
+export const categoriesRelations = relations(category, ({ many }) => ({
+  nomination: many(nomination),
+}));
+
+export const nominee = createTable("nominees", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  slug: text("slug"),
+  name: text("name"),
+  description: text("description"),
+  image: text("image"),
+});
+
+export const nomineeRelations = relations(nominee, ({ many }) => ({
+  primaryNominee: many(nomination, { relationName: "primaryNominee" }),
+  secondaryNominee: many(nomination, { relationName: "secondaryNominee" }),
+}));
+
+export const nomination = createTable(
+  "nominations",
   {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
+    isWinner: boolean("isWinner"),
+    category: uuid("category").references(() => category.id),
+    primaryNominee: uuid("primaryNominee")
+      .references(() => nominee.id)
       .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
+    secondaryNominee: uuid("secondaryNominee").references(() => nominee.id),
   },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
+  (t) => ({
+    pk: primaryKey({ columns: [t.primaryNominee, t.secondaryNominee] }),
+  }),
 );
+
+export const nominationRelations = relations(nomination, ({ one }) => ({
+  primaryNominee: one(nominee, {
+    fields: [nomination.primaryNominee],
+    references: [nominee.id],
+    relationName: "primaryNominee",
+  }),
+  secondaryNominee: one(nominee, {
+    fields: [nomination.secondaryNominee],
+    references: [nominee.id],
+    relationName: "secondaryNominee",
+  }),
+  category: one(category, {
+    fields: [nomination.category],
+    references: [category.id],
+  }),
+}));
