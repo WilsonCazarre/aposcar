6bca3a947e8d6411e36e37e441d67efa1b2338c5
WilsonCazarre <wcs0486@gmail.com>
2025-01-18 04:52:38 -0300

    remove optmistic update from castVote

--- src/components/votes/CastVoteButton.tsx
+++ src/components/votes/CastVoteButton.tsx
@@ -12,27 +12,11 @@ export const CastVoteButton = ({ nominationId, categorySlug }: Props) => {
   const utils = api.useUtils();
 
   const { mutate, isPending } = api.votes.castVote.useMutation({
-    onSuccess: (_, {categorySlug}) => {
-      void utils.nominations.getCategoryWithNavigation.invalidate({categorySlug});
-    },
-    onMutate: ({ categorySlug, nominationId }) => {
-      utils.nominations.getCategoryWithNavigation.setData(
-        { categorySlug },
-        (old) => {
-          if (old) {
-            console.log({ old });
-            const oldNominationIdx = old?.nominations.findIndex(
-              (n) => n.id === nominationId,
-            );
-            if (oldNominationIdx) {
-              console.log({ oldNominationIdx });
-              old.nominations[oldNominationIdx]!.isUserVote = true;
-            }
-          }
-          return old;
-        },
-      );
-    },
+    onSuccess: (_, { categorySlug }) => {
+      void utils.nominations.getCategoryWithNavigation.invalidate({
+        categorySlug,
+      });
+    }
   });
 
   return (
