18cd541ad1c858373e316ce5696a432a13695127
Lucas Felipe <outrolucas2002@gmail.com>
2025-01-18 19:37:56 -0300

    Start profile and adjust social media links

--- src/app/(home)/users/[username]/page.tsx
+++ src/app/(home)/users/[username]/page.tsx
@@ -1,17 +1,54 @@
+import { SocialMediaBadge } from "@/components/SocialMediaBadge";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { db } from "@/server/db";
 import { users } from "@/server/db/schema/auth";
+import { api } from "@/trpc/server";
 import { notFound } from "next/navigation";
 
 const UserPage = async ({ params }: { params: { username: string } }) => {
-  const user = await db.query.users.findFirst({
-    where: (users, { eq }) => eq(users.username, params.username),
-  });
+  const { maxScore, usersScores } = await api.votes.getUserRankings();
+  const winningNominations = await api.nominations.getWinningNominations();
 
-  if (!user) return notFound();
+  const currentUser = usersScores.find(
+    (user) => user.username === params.username,
+  );
+
+  if (!currentUser) return notFound();
 
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-2xl font-bold">{user.username}</h1>
+    <div className="flex flex-col gap-6 lg:flex-row">
+      {/* User info */}
+      <div className="flex flex-col gap-4 lg:w-1/3">
+        {/* Avatar & username */}
+        <div className="flex items-center gap-4">
+          <Avatar className="h-24 w-24">
+            <AvatarImage src={currentUser.profilePic ?? ""} />
+            <AvatarFallback className="text-4xl font-bold">
+              {currentUser.username?.[0]?.toUpperCase() ?? "@"}
+            </AvatarFallback>
+          </Avatar>
+          <h1 className="pt-4 text-2xl font-bold">{currentUser.username}</h1>
+        </div>
+
+        {/* Social medias */}
+        <div className="space-x-2">
+          <SocialMediaBadge
+            url={`https://letterboxd.com/` + currentUser.username}
+            text="Letterboxd"
+          />
+          <SocialMediaBadge
+            url={`https://x.com/` + currentUser.username}
+            text="Twitter"
+          />
+          <SocialMediaBadge
+            url={`https://bsky.app/profile/` + currentUser.username}
+            text="Bluesky"
+          />
+        </div>
+      </div>
+
+      {/* Votes table */}
+      <div className="lg:w-2/3"></div>
     </div>
   );
 };
--- src/components/SocialMediaBadge.tsx
+++ src/components/SocialMediaBadge.tsx
@@ -1,18 +1,26 @@
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import PhArrowUpRight from "~icons/ph/arrow-up-right";
+import PhTwiterLogo from "~icons/ph/twitter-logo";
+import PhDotsThreeOutline from "~icons/ph/dots-three-outline";
+import PhButterfly from "~icons/ph/butterfly";
 
 export function SocialMediaBadge({
   text = "Letterboxd",
   url,
 }: {
-  text: string;
+  text?: string;
   url: string;
 }) {
   return (
-    <Badge variant="outline" className="hover:text-primary">
-      <Link href={url} target="_blank" className="flex items-center gap-1 p-1">
-        {text} <PhArrowUpRight className="h-4 w-4" />
+    <Badge variant="outline" className="group hover:text-primary">
+      <Link href={url} target="_blank" className="flex items-center gap-[6px] p-1">
+        {text === "Twitter" && <PhTwiterLogo className="h-4 w-4" />}
+        {text === "Bluesky" && <PhButterfly className="h-4 w-4" />}
+        {text === "Letterboxd" && <PhDotsThreeOutline className="h-4 w-4" />}
+
+        <p>{text}</p>
+        <PhArrowUpRight className="h-4 w-4 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
       </Link>
     </Badge>
   );
